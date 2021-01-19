9280bdc9bc03640d636636c965c1807d98b4e1fb
Carlos Meneses <mccarlos.en@gmail.com>
2021-01-19 00:41:26 -0500

    Up

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
+  ssr: 'universal',
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
